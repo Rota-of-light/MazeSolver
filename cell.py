d08f2b892e31a043242bbe3de27bb85332119ddd
Rota-of-light <Jeremiah.C.Applebee@gmail.com>
2024-09-06 01:17:16 -0700

    Creating cells and connecting lines

---
+++ cell.py
@@ -0,0 +1,42 @@
+from line import Line
+from point import Point
+
+class Cell():
+    def __init__(self, win):
+        self.has_bottom_wall = True
+        self.has_top_wall = True
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.__x1 = None
+        self.__x2 = None
+        self.__y1 = None
+        self.__y2 = None
+        self.__win = win
+    
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1 #left
+        self.__x2 = x2 #right
+        self.__y1 = y1 #up
+        self.__y2 = y2 #down
+        if self.has_bottom_wall:
+            wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(wall, "black")
+        if self.has_right_wall:
+            wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(wall, "black")
+        if self.has_top_wall:
+            wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(wall, "black")
+        if self.has_left_wall:
+            wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(wall, "black")
+        
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "grey"
+        center_x = (self.__x1 + self.__x2) / 2
+        center_y = (self.__y1 + self.__y2) / 2
+        to_center_x = (to_cell.__x1 + to_cell.__x2) / 2
+        to_center_y = (to_cell.__y1 + to_cell.__y2) / 2
+        self.__win.draw_line(Line(Point(center_x, center_y), Point(to_center_x, to_center_y)), color)
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from window import Window
 from point import Point
 from line import Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    #point_1 = Point(10, 20)
-    #point_2 = Point(20, 20)
-    l = Line(Point(50, 50), Point(400, 400))
+    '''l = Line(Point(50, 50), Point(400, 400))
     new_line = Line(Point(1, 300), Point(500, 300))
     win.draw_line(new_line, "red")
-    win.draw_line(l, "black")
+    win.draw_line(l, "black")'''
+    mehtul_bawkses = Cell(win)
+    mehtul_bawkses.has_bottom_wall = True
+    mehtul_bawkses.has_top_wall = True
+    mehtul_bawkses.has_left_wall = True
+    #mehtul_bawkses.has_right_wall = True
+    mehtul_bawkses.draw(50, 100, 300, 350)
+    mehtul_bawkses2 = Cell(win)
+    mehtul_bawkses2.has_bottom_wall = True
+    mehtul_bawkses2.has_top_wall = True
+    #mehtul_bawkses2.has_left_wall = True
+    mehtul_bawkses2.has_right_wall = True
+    mehtul_bawkses2.draw(310, 100, 560, 350)
+    mehtul_bawkses.draw_move(mehtul_bawkses2)
     win.wait_for_close()
 
 main()
